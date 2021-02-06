a6534d8a9559b978465bbec7fb25788386d75fe8
NoobJsPerson <75886237+NoobJsPerson@users.noreply.github.com>
2021-02-06 17:06:02 +0100

    update deletegame.js

--- commands/deletegame.js
+++ commands/deletegame.js
@@ -7,10 +7,23 @@ module.exports = {
   description:'delete the game you don\'t want to see its runs from the gamelist',
   async execute(message,args){
     if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply('only staff can change game');
-
+        const argz = args.join(' ').split('"');
+    if(argz[2]) args[0] = argz[0].replace(' ','%20');
       const res = await fetch(`https://www.speedrun.com/api/v1/games/${args[0]}`);
-    const json = await res.json();
-    if(!json.data) return message.reply('please input a valid game name or id');
+     let json = await res.json();
+    if(!json.data){
+          
+      const ares = await fetch(`https://www.speedrun.com/api/v1/games?name=${args[0]}`);
+    let ajson = await ares.json();
+    if(!ajson.data) return message.reply('please input a valid name, website name or id ||if the name has spaces put it in ""||');
+    if(ajson.data[0]){
+      json.data = ajson.data.find(x => x.names.international == argz[1])
+      if(!json.data) return message.reply('please input a valid name, website name or id ||if the name has spaces put it in ""||');
+    } else {
+      json = ajson;
+    }
+        } 
+        
    const list = db.get(`${message.guild.id}.game`);
    const nameslist = db.get(`${message.guild.id}.gamenames`);
     if(!list) return message.reply('i can\'t delete a game from a list thats empty');
