c4da67153062e95affc93ed61c2048470a728c97
NoobJsPerson <75886237+NoobJsPerson@users.noreply.github.com>
2021-02-07 17:11:20 +0100

    update addgame.js

--- commands/addgame.js
+++ commands/addgame.js
@@ -7,10 +7,10 @@ module.exports = {
   description:'adds the game you want to see its runs to the gamelist',
   async execute(message,args){
     if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply('only staff can change game');
-        const argz = args.join(' ').split('"');
+    const argz = args.join(' ').split('"');
     if(argz[2]) args[0] = argz[0].replace(' ','%20');
       const res = await fetch(`https://www.speedrun.com/api/v1/games/${args[0]}`);
-        let json = await res.json();
+    let json = await res.json();
     if(!json.data){
           
       const ares = await fetch(`https://www.speedrun.com/api/v1/games?name=${args[0]}`);
@@ -21,10 +21,13 @@ module.exports = {
       if(!json.data) return message.reply('please input a valid name, website name or id ||if the name has spaces put it in ""||');
     } else {
       json = ajson;
+      console.log(json.data);
+
+     if(!json.data.id) return message.reply('please input a valid name, website name or id ||if the name has spaces put it in ""||');
     }
         } 
-        if(db.get(`${message.guild.id}.game`).includes(json.data.id)) return message.reply('i can\'t add a game thats already in the list')
-        
+        if(db.get(`${message.guild.id}.game`) && db.get(`${message.guild.id}.game`).includes(json.data.id)) return message.reply('i can\'t add a game thats already in the list')
+
     db.push(`${message.guild.id}.game`,json.data.id);
     db.push(`${message.guild.id}.gamenames`,json.data.names.international);
     message.channel.send(`the game ${json.data.names.international} has been successfully added to the runlist`);
