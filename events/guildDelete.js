a405aca23996a7d89a45d58959d77ce6d36de25f
NoobJsPerson <75886237+NoobJsPerson@users.noreply.github.com>
2021-03-06 15:19:58 +0100

    update guildDelete.js

---
+++ events/guildDelete.js
@@ -0,0 +1,14 @@
+const fs = require ('fs');
+module.exports = {
+	run(guild){
+	const content = await fs.promises.readFile('./storage.json');
+const storageObject = JSON.parse(content);
+
+
+        if(!storageObject[guild.id]) return;
+
+delete storageObject[guild.id];
+
+await fs.promises.writeFile('./storage.json', JSON.stringify(storageObject));
+	}
+}
