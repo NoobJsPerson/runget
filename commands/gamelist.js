9b1e29d9cc708415561b89337d009895ac43a4f7
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-04-18 18:52:28 +0000

    update gamelist.js

--- commands/gamelist.js
+++ commands/gamelist.js
@@ -5,21 +5,20 @@ module.exports = {
   name:'gamelist',
   description:'displays the list of games that its runs will be sent',
   async execute(message){
-    let total = [];
     const objtype = message.guild?message.guild.id:message.author.id;
     
    function underify(array) {
-     
-     if(`•${array.join('\n•')}`.length <= 2048)return total.push(array);
-     const secondarray = [];
+     if(`•${array.join('\n•')}`.length <= 2048)return [array];
+     let total = [], secondarray = [];
    while(`•${array.join('\n•')}`.length > 2048) secondarray.push(array.shift());
      
      if('•'+secondarray.join('\n•').length <= 2048){
        total.push(secondarray);
      } else {
-       underify(secondarray);
+       total.push(...underify(secondarray));
      }
      total.push(array);
+     return total;
 
    }
     const content = await fs.promises.readFile('./storage.json');
@@ -30,9 +29,9 @@ const list = storageObject[objtype];
     if(!list||!list.length) return message.reply('the gamelist is currently empty add games to it using .addgame');
     const sorted = list.map(x => x.name).sort();
     
-    underify(sorted);
+   let lists = underify(sorted);
     
-    total.forEach(x=>{
+    lists.forEach(x=>{
     message.channel.send({embed:{title:`${message.guild?message.guild.name:message.author.username}'s gamelist`,
       color:'RANDOM',
       description: '•'+x.join('\n•')
