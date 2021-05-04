86b1249ef7c5879b662675ecb22a6cee30150d65
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2021-05-04 21:53:20 +0000

    update duration.js

--- duration.js
+++ duration.js
@@ -5,17 +5,9 @@ module.exports = (time,time2) =>{
  
     let minutes = Math.floor((duration % 360000)/60000);
     let seconds = ((duration % 60000)/1000).toFixed(2);
-    minutes = minutes ? minutes + ' minutes and ' : 
-    if(minutes){
-      minutes+= ' minutes and ';
-    } else {
-      minutes='';
-    }
-      if(hours){
-        hours+=' hours, ';
-      } else {
-        hours='';
-      }
+    minutes = minutes ? minutes + ' minutes and ' : '';
+    
+        hours = hou' hours, ';
 
   return hours+minutes+seconds+' seconds';
   
