ee516cd9ab5155366f199cce188f414fdd4bf4d1
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2022-07-31 19:18:09 +0100

    delete the fs imports

--- commands/deletegame.js
+++ commands/deletegame.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const fs = require('fs');
 module.exports = {
   name: 'deletegame',
   aliases: ['dg'],
--- commands/deletegames.js
+++ commands/deletegames.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const fetch = require('node-fetch');
 module.exports = {
   name: 'deletegames',
