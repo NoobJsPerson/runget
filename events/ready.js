ef304d9ad505b9f5483d7732bada0ad29b4a14da
Amine El Baghdadi <75886237+NoobJsPerson@users.noreply.github.com>
2022-08-01 17:25:52 +0100

    more adjusting

--- events/ready.js
+++ events/ready.js
@@ -134,7 +134,7 @@ module.exports = {
 
 			er = er.filter(x => client.runs.has(x.id));
 			// deleting unnecessary old runs
-		}, 30000);
+		}, 40000);
 		// using setInterval to repeat the process every minute
 	}
 }
